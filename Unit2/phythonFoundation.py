077831fec9008e74a6e838bf80518509488cd323
Terrance Clark <tclark555@boyslatin.net>
2024-10-31 13:37:01 +0000

    Started using pythons, including things like boleans and variables.

---
+++ Unit2/phythonFoundation.py
@@ -0,0 +1,27 @@
+# data types = basic building block of python programming.
+# String = data represented as characters wrapped
+# together in quotation marks.
+
+
+# Interger = whole numbers.
+678
+
+# floats = decimal numbers
+687.99
+
+# bolean = true or false values
+True
+False
+
+# variables = containers for data
+# Steps for a variable
+# 1. create a variable name
+# 2. apply an equal sign
+# 3. give ir a data type value
+
+userName = " My name is Terrance."
+message = 'Good Morning,'
+dayplan =" Today Ill be going trick or treating."
+
+# we use print to see data in our terminal
+print(message + userName + dayplan + ". ")
