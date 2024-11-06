b2c19eb392e28ed351a935d92e0ece7c6d03fbb8
Terrance Clark <tclark555@boyslatin.net>
2024-11-06 14:21:07 +0000

    Asynce assignment done over course of class with Mr. Ian's help.

--- Unit2/asyncActivity_nov1.py
+++ Unit2/asyncActivity_nov1.py
@@ -1,2 +1,9 @@
-int 100
-int 145
+number1=125
+number2=120
+print(number1+number2)
+step2="to create a calender you must use string for the days and integers for the week"
+step3=". a float is a decimal numbers, and intergers for whole numbers"
+step4="the data type for a comment is string, for a survey its boolean"
+print(step2+step3+step4)
+
+I also have nothing that I'd improve the class for.
