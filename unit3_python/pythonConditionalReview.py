dd5b317d9f600bc272f1e916ea4f083e95bc7172
Terrance Clark <tclark555@boyslatin.net>
2024-12-11 14:36:46 +0000

    learning more about printing and a little about the actual date and time command, only briefly though.

---
+++ unit3_python/pythonConditionalReview.py
@@ -0,0 +1,23 @@
+#def welcomeMsgByTime(number, time):
+    #if (time = 'am')
+   # print ('Goodmorning')
+  #  print(str(number)+ time)
+ #   elif time = 'pm'
+#print('good evening')
+#print(str(number)+ time)
+#elif number > 12:
+#print('Sorry, number cant be greater than twelve')
+#else:
+#print('sorry, did not understand your input')
+
+# welcomeMsgByTime(15, 'pm')
+
+
+
+def messageForGrade (grade):
+    if grade = 90
+    print ('Congrats, youve gotten an A' )
+    elif grade = 10
+    print ('Sorry, but you havee failed with a F.')
+messageForGrade()
+    
