1a9b6644348a134ff30a70708ea39ba8abfa98ec
Terrance Clark <tclark555@boyslatin.net>
2025-01-13 14:11:15 +0000

    partially finished async assignment, also got caught without knowing what an argument is but was filled in for it.

---
+++ unit 4/asyncActivityJan10.py
@@ -0,0 +1,12 @@
+FrozenFood=['Waffles, pancakes, vegies, microwave dinners.']
+InterationalFood=['Pizza, 10, sushi, True, 'Guac.']
+ProduceFood['apples, garlic, bell pepper.']
+
+
+
+def traveledPlaces=():
+    place = input("Where have you traveled to? ")   
+    
+    print(place)
+
+traveledPlaces()
