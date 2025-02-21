9599e9e43229ee22bfa9eed1c2608e8c58da0033
Terrance Clark <tclark555@boyslatin.net>
2025-02-21 14:14:06 +0000

    Async Assignment.

---
+++ async/asyncActivityFeb21.py
@@ -0,0 +1,13 @@
+What is the name of your game?
+# The name of the game is Hunter X Elite.
+What genre is your game going to be in (horror, action-adventure, shooting, fighting, platformer, etc.)
+# The game will be an action/adventure game.
+What is the objective and general game loop of your game? Please write your response in complete sentences. example: players will try to score points by hitting randomized targets in a set amount of time.
+# The objective of the game is to have an open world action adventure game with levels and skills all based on the anime franchise hunter x hunter.
+What already existing games does your game take inspiration from? Please provide at least 3 examples, including the name of the game, and describe the aspect your inspired by in complete sentences.
+# Bloxfruits, a game made on roblox.
+Please describe the mechanics, or functional parts that will be in your game example:
+
+a. my game will allow the player to move across the screen horizontally
+b. my game will feature different weapon types that will have different effects.
+# B, the game will feature a lot of different moves and effects.
---
+++ unit 4/asyncActivityJan17.py
@@ -0,0 +1,15 @@
+# Using the list above, create a function that will reverse the order of the list/array.
+# Hint: there is a method that you can use to accomplish this.
+
+# Using the list above, Create a function that will add an item that you would bring to a camping trip to your list/array.
+
+# Create a function the will combine the campingSupplies list/ array, with the list/array provided below:
+
+
+
+# Create a function that will remove the "flash light" item from the list/array and replace it with a "camp fire kit" item.
+
+
+
+campingFood = ['marshmellows','gram crackers','chocolate','chicken hot dogs','water',]
+    print(campingFood)
